c784a745858d784a9432453369b09b37c5ae1c0b
bhanum1 <132242829+bhanum1@users.noreply.github.com>
2023-11-21 16:03:36 -0500

    stuff

--- datacleanup.py
+++ datacleanup.py
@@ -6,6 +6,7 @@ import random
 import pandas
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 # Get data into list
 PATH = '/Users/bhanumamillapalli/Documents/GitHub/APS360_Project_Music_Prediction'
@@ -17,9 +18,12 @@ with open(PATH + '/maestro-v3.0.0/maestro-v3.0.0.csv') as csvfile:
 
 
 data_points = []
+unnorm_data_points = []
 labels = []
+unnorm_labels = []
 # Convert sample file into pm object
-sample_size = 100
+sample_size = 50
+num_samples = 100
 
 #generate empty lists
 pitches = []
@@ -39,7 +43,11 @@ for file in data[1:]:
     instrument = pm.instruments[0]
 
     #generate set of indices of random notes
-    indices = random.sample(range(0,len(instrument.notes)-(sample_size+ 1)),10)
+    try:
+        indices = random.sample(range(0,len(instrument.notes)-(sample_size+ 1)),num_samples)
+    except:
+        indices = [0] * num_samples
+        print('Short', len(instrument.notes))
 
     for index in indices:
         sample_pitches = []
@@ -77,40 +85,64 @@ normalized_starts = scaler.fit_transform(np.array(starts).reshape(-1,1))
 normalized_durations = scaler.fit_transform(np.clip(np.array(durations).reshape(-1,1), 0,1))
 
 
-figure1 = plt.figure()
-figure1
-line2, = plt.plot(normalized_starts, label="Normalized start times")
-line3, = plt.plot(normalized_durations, label = "Normalized durations")
-leg = plt.legend(loc='best')
-plt.title("After Normalization")
-plt.show()
-
 notes = []
+unnorm_notes = []
 for i in range(len(pitches)):
     if (i+1) % (sample_size+1) == 0:
         data_points.append(notes)
+        unnorm_data_points.append(unnorm_notes)
         notes = []
-        labels.append([pitches[i], normalized_starts[i][0], normalized_durations[i][0]])
+        unnorm_notes = []
+        labels.append([pitches[i], normalized_starts[i], normalized_durations[i]])
+        unnorm_labels.append([pitches[i], starts[i], durations[i]])
     else:
-        notes.append([pitches[i], normalized_starts[i][0], normalized_durations[i][0]])
+        notes.append([pitches[i], normalized_starts[i], normalized_durations[i]])
+        unnorm_notes.append([pitches[i], starts[i], durations[i]])
 
 dataset = torch.FloatTensor(data_points)
 labels = torch.FloatTensor(labels)
+unnorm_dataset = torch.FloatTensor(unnorm_data_points)
+unnorm_labels = torch.FloatTensor(unnorm_labels)
 
+torch.save(dataset, PATH + '/data/dataset.pt')
+torch.save(labels, PATH + '/data/labels.pt')
+torch.save(unnorm_dataset, PATH + '/data/unnorm_dataset.pt')
+torch.save(unnorm_labels, PATH + '/data/unnorm_labels.pt')
+
+
+
+
+#if you need to write as a .csv
 '''
-
-torch.save(dataset, PATH + '/dataset.pt')
-torch.save(labels, PATH + '/labels.pt')
-
-with open(PATH + '/dataset.csv', 'w') as f:
+with open(PATH + '/data/dataset.csv', 'w') as f:
      
     # using csv.writer method from CSV package
     write = csv.writer(f)
     write.writerows(data_points)
 
-with open(PATH +'/labels.csv', 'w') as f:
+with open(PATH +'/data/labels.csv', 'w') as f:
      
     # using csv.writer method from CSV package
     write = csv.writer(f)
     write.writerows(labels)
 '''
+
+# Split the data into train/validation/test with 60/20/20 splits
+X_train, X_test, y_train, y_test = train_test_split(dataset,labels,test_size=0.2,random_state=1)
+X_train, X_val, y_train, y_val = train_test_split(X_train,y_train,test_size=0.25,random_state=1)
+
+# Check the sizes to make sure they're right
+print("Training Data: ", X_train.shape)
+print("Training Labels: ", y_train.shape)
+print("Validation Data: ", X_val.shape)
+print("Validation Labels: ", y_val.shape)
+print("Testing Data: ", X_test.shape)
+print("Testing Labels: ", y_test.shape)
+
+# Save all the info
+torch.save(X_train, PATH + '/data/train_data.pt')
+torch.save(y_train, PATH + '/data/train_labels.pt')
+torch.save(X_val, PATH + '/data/val_data.pt')
+torch.save(y_val, PATH + '/data/val_labels.pt')
+torch.save(X_test, PATH + '/data/test_data.pt')
+torch.save(y_test, PATH + '/data/test_labels.pt')
